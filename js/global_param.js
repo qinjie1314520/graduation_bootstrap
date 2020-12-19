dbf23bcbdc2cb07d8d8ba9ac0ed1d111a77cca4d
qj18780122553@163.com <qj18780122553>
2020-12-19 22:04:41 +0800

    [ADD]

---
+++ js/global_param.js
@@ -0,0 +1,14 @@
+export const common = {
+	token: "token"
+}
+export const address_of_the_interface = {
+	url_base:"http://139.9.87.17:30004",
+	url_project_authority:"/graduation-project-roger-authority",
+	 url_project_article:"/graduation-project-roger-article",
+	 url_login:"/authority/login",
+	 url_selArticleType:"/article/home/selArticleType",
+	 url_selLatestArticle:"/article/home/selLatestArticle",
+	 url_selHotArticle:"/article/home/selHotArticle",
+	 url_selDailyRecommendation:"/article/home/selDailyRecommendation",
+	 url_selDailySentence:"/article/home/selDailySentence"
+}
